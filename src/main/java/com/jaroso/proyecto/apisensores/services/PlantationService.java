fe70cddb16b164bb4c0e4620c5b53a6417b345e9
alxrgdriguez <sralexred@gmail.com>
2025-01-31 17:27:22 +0100

    Entity Plantantion

--- src/main/java/com/jaroso/proyecto/apisensores/services/PlantationService.java
+++ src/main/java/com/jaroso/proyecto/apisensores/services/PlantationService.java
@@ -1,4 +1,16 @@
 package com.jaroso.proyecto.apisensores.services;
 
-public class PlantationService {
+import com.jaroso.proyecto.apisensores.entities.Plantation;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PlantationService {
+
+    public Plantation getPlantation(Long id);
+    public List<Plantation> getAllPlantations();
+    public void deletePlantationById(Long id);
+    public void savePlantation(Plantation plantation);
+
 }

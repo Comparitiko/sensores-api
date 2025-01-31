6ab05f32eb7a4f5c4a04512c1a56035b21ae4e71
alxrgdriguez <sralexred@gmail.com>
2025-01-31 17:07:26 +0100

    Entity plantation completed

--- src/main/java/com/jaroso/proyecto/apisensores/entities/Plantation.java
+++ src/main/java/com/jaroso/proyecto/apisensores/entities/Plantation.java
@@ -1,10 +1,18 @@
 package com.jaroso.proyecto.apisensores.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "plantatios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Plantation {
 
+    // Attributes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -13,8 +21,9 @@ public class Plantation {
 
     private String ubication;
 
+    private String coordinates;
+
     @Column(name = "plantation_type")
     private String plantationType;
-
 
 }

d9d4a2ebfc8e41df851a187fde1425f82fabc6a2
Comparitiko <gabigcy@gmail.com>
2025-02-07 16:26:34 +0100

    Hecho metodo de registrar usuarios haciendo comprobaciones

--- src/main/java/com/jaroso/proyecto/apisensores/services/UserServiceImpl.java
+++ src/main/java/com/jaroso/proyecto/apisensores/services/UserServiceImpl.java
@@ -3,8 +3,11 @@ package com.jaroso.proyecto.apisensores.services;
 import com.jaroso.proyecto.apisensores.dto.UserRegisterDTO;
 import com.jaroso.proyecto.apisensores.entities.User;
 import com.jaroso.proyecto.apisensores.repositories.UserRepository;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -29,11 +32,46 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public User save(UserRegisterDTO userRegisterDTO) {
-    User user = new User();
-    user.setUsername(userRegisterDTO.getUsername());
-    user.setEmail(userRegisterDTO.getEmail());
-    //Falta comprobación de que password2 es igual a password
-    user.setPassword(this.passwordEncoder.encode(userRegisterDTO.getPassword()));
-    return this.userRepository.save(user);
+    // Check if user already exists
+    Optional<User> user = this.userRepository.findUserByUsernameOrEmail(
+      userRegisterDTO.getUsername(),
+      userRegisterDTO.getEmail()
+    );
+
+    if (user.isPresent()) {
+      throw new ResponseStatusException(
+        HttpStatus.BAD_REQUEST,
+        "User already exist"
+      );
+    }
+
+    // Check if passwords match
+    if (!userRegisterDTO.getPassword().equals(userRegisterDTO.getConfirmPassword())) {
+      throw new ResponseStatusException(
+        HttpStatus.BAD_REQUEST,
+        "Passwords do not match"
+      );
+    }
+
+    // Create new user
+    User newUser = new User();
+    newUser.setUsername(userRegisterDTO.getUsername());
+    newUser.setEmail(userRegisterDTO.getEmail());
+    newUser.setPassword(this.passwordEncoder.encode(userRegisterDTO.getPassword()));
+
+    // Save user to database catching OptimisticLockingFailureException and any other exception
+    try {
+      return this.userRepository.save(newUser);
+    } catch (OptimisticLockingFailureException e) {
+      throw new ResponseStatusException(
+        HttpStatus.BAD_REQUEST,
+        "User already exist"
+      );
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        "Error saving user"
+      );
+    }
   }
 }

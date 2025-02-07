bcb9f7667bb465a104b2d932fb19c94c0f12c2a5
Comparitiko <gabigcy@gmail.com>
2025-02-07 19:35:35 +0100

    Modificado sensor dto

--- src/main/java/com/jaroso/proyecto/apisensores/dto/SensorDTO.java
+++ src/main/java/com/jaroso/proyecto/apisensores/dto/SensorDTO.java
@@ -3,41 +3,25 @@ package com.jaroso.proyecto.apisensores.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jaroso.proyecto.apisensores.enums.SensorType;
 import com.jaroso.proyecto.apisensores.enums.Unit;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class SensorDTO {
+  @JsonProperty("sensor_type")
   private SensorType sensorType;
+
   private String location;
+
   private Double latitude;
+
   private Double longitude;
+
   private Unit unit;
+
+  @JsonProperty("plantation_id")
   private Long plantationId;
-
-  public SensorDTO(
-    @JsonProperty("sensor_type")
-    SensorType sensorType,
-
-    String location,
-
-    Double latitude,
-
-    Double longitude,
-
-    Unit unit,
-
-    @JsonProperty("plantation_id")
-    Long plantationId
-  ) {
-    this.sensorType = sensorType;
-    this.location = location;
-    this.latitude = latitude;
-    this.longitude = longitude;
-    this.unit = unit;
-    this.plantationId = plantationId;
-  }
-
-
 }

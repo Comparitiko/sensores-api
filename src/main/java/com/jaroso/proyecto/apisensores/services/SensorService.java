086e460e8ff188597b7c43390b084d3e4e54a147
AdrianPerez2207 <aperezsanchez441@gmail.com>
2025-02-07 18:58:25 +0100

    sensorResponse modificado

--- src/main/java/com/jaroso/proyecto/apisensores/services/SensorService.java
+++ src/main/java/com/jaroso/proyecto/apisensores/services/SensorService.java
@@ -5,18 +5,19 @@ import com.jaroso.proyecto.apisensores.dto.SensorDataDTO;
 import com.jaroso.proyecto.apisensores.dto.SensorDTO;
 import com.jaroso.proyecto.apisensores.enums.SensorType;
 import com.jaroso.proyecto.apisensores.entities.Sensor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public interface SensorService {
-  List<Sensor> getAllSensors();
-  Sensor saveSensor(SensorDTO sensor);
-  Sensor getSensorById(Long id);
-  List<FluxTable> getDataBySensor(Long id);
-  List<Sensor> getSensorsByType(SensorType sensorType);
-  SensorDataDTO saveDataOfSensor(Long sensorId, SensorDataDTO sensorDataDto);
-  List<FluxTable> getDataByLocation(String location);
-  Sensor deleteSensorById(Long id);
+  ResponseEntity<?> getAllSensors();
+  ResponseEntity<?> saveSensor(SensorDTO sensor);
+  ResponseEntity<?> getSensorById(Long id);
+  ResponseEntity<?> getDataBySensor(Long id);
+  ResponseEntity<?> getSensorsByType(SensorType sensorType);
+  ResponseEntity<?> saveDataOfSensor(Long sensorId, SensorDataDTO sensorDataDto);
+  ResponseEntity<?> getDataByLocation(String location);
+  ResponseEntity<?> deleteSensorById(Long id);
 }

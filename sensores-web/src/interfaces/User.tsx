45f791fa6aaeb647f6b3a0ac5b073f8b3ec2497a
missymogara <monicagara97@gmail.com>
2025-02-21 20:52:17 +0100

    basic user

--- sensores-web/src/interfaces/User.tsx
+++ sensores-web/src/interfaces/User.tsx
@@ -1,9 +1,5 @@
-import { Plantation } from '../interfaces/Plantation';
+// import { Plantation } from '../interfaces/Plantation';
 export interface User {
-    id: string;
     username: string;
-    password: string;
-    email: string;
     token: string;
-    plantations: Plantation[];
 }
